517a33b0692accd80711978bcf01cb089882e395
NerimuZ <0ps24917788528a@ezweb.ne.jp>
2025-09-22 14:45:47 +0900

    pqpqpqpqpq

--- MagicMuscle/Assets/Script/Tutorial/Tutorial_GameManager.cs
+++ MagicMuscle/Assets/Script/Tutorial/Tutorial_GameManager.cs
@@ -2,8 +2,25 @@ using UnityEngine;
 
 public class Tutorial_GameManager : MonoBehaviour
 {
+<<<<<<< Updated upstream
     private bool IsSteckUp = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+=======
+    
+    // 複数のステップを管理できるようにリスト化
+    public List<TutorialStep> tutorialSteps = new List<TutorialStep>();
+    public TextMeshProUGUI TM;
+    private int currentStepIndex = 0;
+
+    private Serial serial;
+    public SliderCharge slider;
+    private bool isMoveTextTimer = false;
+    public string scenename;
+    // クラスのメンバに追加
+    public AudioSource audioSource;   // 音声再生用
+    public AudioClip[] voiceClips;    // 各セリフに対応する音声
+
+>>>>>>> Stashed changes
     void Start()
     {
         
@@ -14,6 +31,12 @@ public class Tutorial_GameManager : MonoBehaviour
     {
         if (IsSteckUp)
         {
+            if (audioSource != null && voiceClips != null && currentStepIndex < voiceClips.Length)
+        {
+            audioSource.Stop();
+            audioSource.clip = voiceClips[currentStepIndex];
+            audioSource.Play();
+        }
 
         }
     }
